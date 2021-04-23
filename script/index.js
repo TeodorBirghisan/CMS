56de94b274a478a5a1ad4c3c9df528a82b050518
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-23 11:40:12 +0300

    Get all data from forms

--- script/index.js
+++ script/index.js
@@ -3,8 +3,9 @@ function validateForm() {
   let prenume = document.forms["myForm"]["fprenume"].value;
   let email = document.forms["myForm"]["femail"].value;
   let chooseFile = document.forms["myForm"]["img"].value;
+  let sex = document.forms["myForm"]["sex"].value;
+  let date = document.forms["myForm"]["trip-start"].value;
   if (chooseFile == "" || nume == "" || prenume == "" || email == "") {
     alert("Don't leave empty fields");
-    return false;
   }
 }

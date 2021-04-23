702fbfe780737f463e43d8d77bb66667102255ef
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-23 11:31:29 +0300

    Verify if any of the fields are empty on submit

--- script/index.js
+++ script/index.js
@@ -1,7 +1,10 @@
 function validateForm() {
-  let x = document.forms["myForm"]["fnume"].value;
-  if (x == "") {
-    alert("Name must be filled out");
+  let nume = document.forms["myForm"]["fnume"].value;
+  let prenume = document.forms["myForm"]["fprenume"].value;
+  let email = document.forms["myForm"]["femail"].value;
+  let chooseFile = document.forms["myForm"]["img"].value;
+  if (chooseFile == "" || nume == "" || prenume == "" || email == "") {
+    alert("Don't leave empty fields");
     return false;
   }
 }
